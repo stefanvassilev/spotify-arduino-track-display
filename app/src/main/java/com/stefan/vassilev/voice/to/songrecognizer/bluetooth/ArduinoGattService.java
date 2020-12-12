4793270d8a0fe78a373804b55e364a9f279a66c8
Stefan Vassilev <stefanvassilev1@gmail.com>
2020-12-12 11:55:30 +0200

    WIP Implement notification for recieving of audio

--- app/src/main/java/com/stefan/vassilev/voice/to/songrecognizer/bluetooth/ArduinoGattService.java
+++ app/src/main/java/com/stefan/vassilev/voice/to/songrecognizer/bluetooth/ArduinoGattService.java
@@ -1,12 +1,20 @@
 package com.stefan.vassilev.voice.to.songrecognizer.bluetooth;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.util.Log;
 
 import com.polidea.rxandroidble2.RxBleConnection;
 import com.polidea.rxandroidble2.RxBleDevice;
 import com.polidea.rxandroidble2.utils.ConnectionSharingAdapter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -19,6 +27,17 @@ public class ArduinoGattService {
     private static final String TAG = ArduinoGattService.class.getName();
 
     private static final String ARDUINO_SERVICE = "5bcc5572-2783-11eb-adc1-0242ac120002";
+    private static final String ARDUINO_SERVICE_AUDIO_SUBSCRIBE = "60e45b02-397d-11eb-adc1-0242ac120002";
+    private static final long RECORD_TIME = 10000;
+    private final MediaPlayer mp = new MediaPlayer();
+
+
+    int[] sampledAudio = new int[2000];
+    boolean fullArray;
+    int counter = 0;
+
+    File wavFile = new File(Environment.getExternalStorageDirectory() + "/" + File.separator + "test.wav");
+    //TODO: Continue form here https://stackoverflow.com/questions/17192256/recording-wav-with-android-audiorecorder
 
     private final Activity activity;
     private final RxBleDevice bluetoothDevice;
@@ -40,12 +59,23 @@ public class ArduinoGattService {
         }
 
 
-        while (true) {
-            toggleLEDIndefinetely(1);
-            toggleLEDIndefinetely(0);
-        }
+        receiveAudio();
 
+//        while (true) {
+//            toggleLEDIndefinetely(1);
+//            toggleLEDIndefinetely(0);
+//        }
+
+//        Disposable d = rxBleConnectionObservable
+//                .flatMap(rxBleConnection -> rxBleConnection.setupNotification(UUID.fromString(ARDUINO_SERVICE_AUDIO_SUBSCRIBE)))
+//                .doOnNext(notificationObservable -> {
+//                })
+//                .flatMap(notificationObservable -> notificationObservable)
+//                .subscribe(bytes -> {
+//                    Log.e(TAG, "Info" + Arrays.toString(bytes));
+//                });
     }
+
 
     private void toggleLEDIndefinetely(int val) {
         try {
@@ -73,6 +103,65 @@ public class ArduinoGattService {
         }
 
 
+    }
+
+    private void playAudio() {
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(activity.getApplicationContext().openFileOutput("config.wav", Context.MODE_PRIVATE));
+            for (int value : sampledAudio) {
+                outputStreamWriter.write(value);
+            }
+            outputStreamWriter.close();
+            FileInputStream fileInputStream = activity.getApplicationContext().openFileInput("config.wav");
+//            FileInputStream rawmp3file= new FileInputStream("config.wav");
+
+            mp.setDataSource(fileInputStream.getFD());
+            mp.prepare();
+            mp.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private InputStream receiveAudio() {
+        //TODO: Read on: https://github.com/Polidea/RxAndroidBle#change-notifications
+        Disposable subscribe = rxBleConnectionObservable
+                .flatMap(rxBleConnection -> rxBleConnection.setupNotification(UUID.fromString(ARDUINO_SERVICE_AUDIO_SUBSCRIBE)))
+                .doOnNext(notificationObservable -> {
+
+                    notificationObservable.subscribe(bytes -> {
+                        Log.i(TAG, "Received: " + Arrays.toString(bytes));
+                    }, throwable -> Log.e(TAG, "Failed", throwable)).dispose();
+                })
+                .flatMap(notificationObservable -> notificationObservable) // <-- Notification has been set up, now observe value changes.
+                .subscribe(
+                        bytes -> {
+                            if (counter % 100 == 0) {
+                                Log.w(TAG, "Receiving data");
+                            }
+
+//                            Log.w(TAG, "wrote 20k successfully");
+                            if (counter == sampledAudio.length) {
+                                Log.w(TAG, "wrote 20k successfully");
+                                playAudio();
+                                counter = 0;
+                            }
+                            sampledAudio[counter++] = bytes[0];
+
+                            // Characteristic value confirmed.
+
+                            // Given characteristic has been changes, here is the value.
+                        },
+                        throwable -> {
+                            Log.e(TAG, "failed while receiving bytes: ", throwable);
+                            // Handle an error here.
+                        }
+                );
+
+        return null;
     }
 
 
